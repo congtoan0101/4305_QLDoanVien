fc47dee6459c7c21c0a82eefac748c8b160f485d
Trần Văn Dân <tranvandan2707@gmail.com>
2017-07-14 23:27:44 +0700

    Xong chức năng Đoàn cơ sở

--- application/controllers/Doancs.php
+++ application/controllers/Doancs.php
@@ -6,6 +6,10 @@ class Doancs extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		$this->load->model('Model_Doancs');
+		if ($this->session->userdata('QUYEN') < 1) {
+			@header("Location: " . base_url());
+			@die;
+		}
 	}
 
 	public function index() {
@@ -20,6 +24,50 @@ class Doancs extends CI_Controller {
 		];	
 
 		$this->load->view('mainLayout', $data);
+	}
+
+	public function sua($maDCS = "") {
+		if (!empty($maDCS)) {
+			$mdDCS = $this->Model_Doancs;
+			$doancs = $mdDCS->get($maDCS);
+			if ($doancs == null) {
+				@header("Location: " . base_url('doancs'));
+				@die;
+			}
+			$data = [
+				'metaTitle' => 'Sửa thông tin | Đoàn cơ sở',
+				'mainContent' => 'doancs/sua',
+				'sb_page' => 'doancs',
+				'doancs' => $doancs
+			];	
+
+			$this->load->view('mainLayout', $data);
+		} else {
+			@header("Location: " . base_url('doancs'));
+			@die;
+		}
+	}
+
+	public function xoa($maDCS = "") {
+		if (!empty($maDCS)) {
+			$mdDCS = $this->Model_Doancs;
+			$doancs = $mdDCS->get($maDCS);
+			if ($doancs == null) {
+				@header("Location: " . base_url('doancs'));
+				@die;
+			}
+			$data = [
+				'metaTitle' => 'Xóa | Đoàn cơ sở',
+				'mainContent' => 'doancs/xoa',
+				'sb_page' => 'doancs',
+				'doancs' => $doancs
+			];	
+
+			$this->load->view('mainLayout', $data);
+		} else {
+			@header("Location: " . base_url('doancs'));
+			@die;
+		}
 	}
 
 	// Xử lý ajax
@@ -59,5 +107,70 @@ class Doancs extends CI_Controller {
 		}
 
 		echo json_encode($result);
+	}
+
+	function xulySuaDCS() {
+		$mdDCS = $this->Model_Doancs;
+		$params = $this->input->post();
+
+		if (!isset($params['MADCS']) || empty($params['MADCS'])) {
+			$result = array(
+				'status' => false,
+				'message' => 'Chưa nhập Mã Đoàn cơ sở'
+			);
+		} elseif (!isset($params['TENDCS']) || empty($params['TENDCS'])) {
+			$result = array(
+				'status' => false,
+				'message' => 'Chưa nhập Tên Đoàn cơ sở'
+			);
+		} else {
+			if (!$mdDCS->existed($params['MADCS'])) {
+				$result = array(
+					'status' => false,
+					'message' => 'Mã Đoàn cơ sở này không tồn tại. Vui lòng kiểm tra lại.'
+				);
+			} else {
+				if ($mdDCS->update($params)) {
+					$result = array(
+						'status' => true,
+						'message' => 'Cập nhật Đoàn cơ sở thành công'
+					);
+				} else {
+					$result = array(
+						'status' => false,
+						'message' => 'Lỗi khi cập nhật Đoàn cơ sở'
+					);
+				}
+			}
+		}
+
+		echo json_encode($result);
+	}
+
+	function xulyXoaDCS() {
+		$mdDCS = $this->Model_Doancs;
+		$maDCS = $this->input->post('MADCS');
+		
+		if (!$mdDCS->existed($maDCS)) {
+			$result = array(
+				'status' => false,
+				'message' => 'Mã Đoàn cơ sở này không tồn tại. Vui lòng kiểm tra lại.'
+			);
+		} else {
+			if ($mdDCS->delete($maDCS)) {
+				$result = array(
+					'status' => true,
+					'message' => 'Xóa Đoàn cơ sở thành công'
+				);
+			} else {
+				$result = array(
+					'status' => false,
+					'message' => 'Lỗi khi xóa Đoàn cơ sở'
+				);
+			}
+		}
+
+		echo json_encode($result);
+		die;
 	}
 }
